fbcc5af3dd9a4493bb12e73bc0d9fa6779a113f4
ninatuddao <b2029jmdtuddao@pshs.edu.ph>
2026-01-20 14:32:09 +0800

    i mean i tried my best

--- public/js/quiz.js
+++ public/js/quiz.js
@@ -1,23 +1,23 @@
 const questions = [
   {
-    text: "Question 1 (placeholder)",
-    options: ["Option A", "Option B", "Option C", "Option D"],
-    values: ["A", "B", "C", "D"]
+    text: "Q1: What is your favorite game in Alice in Borderland?",
+    options: ["7 of Hearts (Hide and Seek)", "Queen of Spades (Checkmate)", "King of Diamonds (Beauty Contest)", "10 of Hearts (Witch Hunt)", "All of them!", "I don't care...just move on...", "King of Clubs (Osmosis)"],
+    values: ["Arisu", "Usagi", "Chishiya", "Kuina", "Niragi", "Ann", "Tatta"]
   },
   {
     text: "Question 2 (placeholder)",
     options: ["Option A", "Option B", "Option C", "Option D"],
-    values: ["A", "B", "C", "D"]
+    values: ["Arisu", "Usagi", "Chishiya", "Kuina", "Niragi", "Ann", "Tatta"]
   },
   {
     text: "Question 3 (placeholder)",
     options: ["Option A", "Option B", "Option C", "Option D"],
-    values: ["A", "B", "C", "D"]
+    values: ["Arisu", "Usagi", "Chishiya", "Kuina", "Niragi", "Ann", "Tatta"]
   }
 ];
 
 let currentQuestion = 0;
-let scores = { A: 0, B: 0, C: 0, D: 0 };
+let scores = { Arisu: 0, Usagi: 0, Chishiya: 0, Kuina: 0, Niragi: 0, Ann: 0, Tatta: 0 };
 
 const quizBox = document.getElementById("quiz-box");
 const questionText = document.getElementById("question-text");
@@ -52,8 +52,9 @@ function selectAnswer(choice) {
 
 function showResult() {
   quizBox.classList.add("hidden");
+  
 
-  let highest = "A";
+  if (highest = "Arisu"){
   for (let key in scores) {
     if (scores[key] > scores[highest]) highest = key;
   }
@@ -81,10 +82,44 @@ let resultHTML = `
     </p>
   </div>
 `;
-
-
   resultDiv.innerHTML = resultHTML;
   resultDiv.classList.remove("hidden");
-}
 
+  }
+  
+  else if(highest = "Usagi"){
+  for (let key in scores) {
+    if (scores[key] > scores[highest]) highest = key;
+  }
+
+let resultHTML = `
+  <div class="result-header">
+    <div class="result-name">
+      <h3>USAGOIII</h3>
+    </div>
+
+    <div class="result-image">
+      
+      <img src="characs/arisu.jpg" alt="Arisu">
+    </div>
+  </div>
+
+  <div class="result-description">
+    <p>
+      (Placeholder blablabla bajalnaaobakbkbjojbdasbduahdiaonduiasbdioahdouashd 
+      ur literally niragi)
+    </p>
+
+    <p class="result-summary">
+      (Placeholder blabla niragi whatever okay bye)
+    </p>
+  </div>
+`;
+  resultDiv.innerHTML = resultHTML;
+  resultDiv.classList.remove("hidden");
+
+  }}
 loadQuestion();
+
+
+
